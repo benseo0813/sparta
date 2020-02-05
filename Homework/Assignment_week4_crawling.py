47b36a775f2456c3a37c2fe10d9d76c0e83d9e27
benseo0813 <59481984+benseo0813@users.noreply.github.com>
2020-02-06 00:07:31 +0900

    Add files via upload

---
+++ Homework/Assignment_week4_crawling.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
+
+# URL을 읽어서 HTML를 받아오고,
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20190908',headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+soup = BeautifulSoup(data.text, 'html.parser')
+
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.title.ellipsis
+# select를 이용해서, tr들을 불러오기
+songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+rank = 1
+for song in songs:
+    
+    a_tag = song.select_one('td.info > a.title')
+    if a_tag is not None:
+        title = a_tag.text
+        artist = song.select_one('a.artist').text
+
+        doc = {
+            'rank': rank,
+            'title': title,
+            'artist': artist
+        }
+        db.songs.insert_one(doc)
+        rank += 1
+        
+     # 질문 1. print를 해보면 영화 랭킹과 노래 제목 사이에 엄청 긴 빈 칸이 생기는데 이것은 크롤링할 때 포맷이 원래 그런건가요? 아니면 수정이 가능한 것일까요?
